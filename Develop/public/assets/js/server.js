5b1a734c0bbe6bd662d9a16e7ec9ae774ec55368
Phillip Dunham <phillipwwd@gmail.com>
2020-01-10 22:09:15 -0800

    api post works

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const notesapi = require("../../../db/db.json");
+
 const app = express();
+const readFileAsync = util.promisify(fs.readFile);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const PORT = process.env.PORT  || 3000;
 
-app.get("/", function(req, res){
+app.get("/", function(req, res){                        //Sends homepage, index.html
     res.sendFile(path.join(__dirname,"../../index.html"))
 })
 
-app.listen(PORT,function(req,res){
-    console.log("listening on port " + PORT);
+app.get("/notes", function(req, res){                  //Sends notes page, notes.html
+    res.sendFile(path.join(__dirname,"../../notes.html"))
+})
+
+app.get("/api/notes", function(req, res){//async function(req,res){                //sends JSON objects
+    res.json(notesapi)
+})
+
+app.post("/api/notes", function(req, res){
+    notesapi.push(req.body);
+    res.json(true);
+})
+
+app.delete("/api/notes", function(req, res){
     
+})
+
+app.listen(PORT,function(req,res){                      //Logs that server is running
+    console.log("listening on port " + PORT);    
 });
